71fc6ac5d9a753906980b4037c0f05e0c4f664b2
wonghye <dearhw117@naver.com>
2022-06-15 18:20:05 +0900

    day25

--- hi-tour/js/common.js
+++ hi-tour/js/common.js
@@ -149,12 +149,12 @@ $(document).ready( function() {
 		$('header').addClass('on');
 		$('header .header_cont').slideDown('fast');
 		$('header .header_area .header_cont .closePop').show();			
-		$("body").bind('touchmove', function(e){e.preventDefault()});
+		//$("body").bind('touchmove', function(e){e.preventDefault()});
 	});
 	$('header .header_cont .closePop').click(function(){
         $('.header_cont').slideUp('fast');
         $('header').removeClass('on');
-        $("body").unbind('touchmove');
+        //$("body").unbind('touchmove');
 	});
 	
 });
